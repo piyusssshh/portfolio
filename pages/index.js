2cf5968087328637e9685d62f0279fb05d7af64a
piyussssh <mvps1862@gmail.com>
2023-02-20 20:47:45 +0530

    backup

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ const index = () => {
      <div className="hero-cont">
      <h1 className="heading-one">HELLO I'M PIYUSH</h1>
      <p className="p-one"> offer UI/UX, web design, development, and graphic design services with a focus on user-centered designs. Explore my portfolio to see my skills and creativity in action</p>
+     <button className="btn-prime">EXPLORE WORKS</button>
      </div>
 
     </div>
