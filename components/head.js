1f842617cc15827ab3965b9d460a5ca2ed82e7fc
piyussssh <mvps1862@gmail.com>
2023-06-10 13:26:17 +0530

    meta updated

--- components/head.js
+++ components/head.js
@@ -4,6 +4,8 @@ const HeadTag =()=> {
     <>
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>    
+    <title>GFX PIYUSH - UI & UX Designer, Front-End Developer</title>
+    <meta name="description" content="Welcome to GFX Piyush, where stunning designs meet seamless functionality. Our expert team specializes in creating visually captivating and user-friendly websites. Discover the perfect blend of artistry and technology as we transform your vision into an engaging online experience. Let us elevate your digital presence with our cutting-edge front-end development solutions. Explore GFX Piyush today and unlock the potential of captivating web design."></meta>
     <link rel="icon" href="/fav.png"/>   
   </Head>
     </>
